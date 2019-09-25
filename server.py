51da7c50dca07e199ff48e79b4a7c383e56de537
MountGuy <riman314@snu.ac.kr>
2019-09-25 21:27:58 +0900

    prepare next game after game over

--- server.py
+++ server.py
@@ -1,22 +1,22 @@
 import random
-import threading
-import socket as sck
 
+from threading import Thread
 from player import Player
 from room import Room
-from setting import send, recieve
+from protocol import send, recieve, newSocket
 
 class Server :
   def __init__(self):
     self.room = Room('room #1')
-    self.serverSock = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
+    self.serverSock = newSocket()
     self.serverSock.bind(('', 8080))
     self.serverSock.listen(5)
     self.threads = []
     self.sockets = []
     self.roomNum = 1
     self.dict = {}
-    self.turn = random.randrange(0, 4)
+    self.playerN = 2
+    self.turn = random.randrange(0, self.playerN - 1)
 
   def boardToString(self, board):
     string = ['', '', '', '', '']
@@ -37,18 +37,23 @@ class Server :
       if parse[0] == 'PICK':
         print(self.turn, ' ', i)
         if i == self.turn:
-          self.turn = (self.turn + 1) % 5
+          self.turn = (self.turn + 1) % self.playerN
           self.room.pickNumber(int(parse[1]))
           self.room.printStatus()
           self.distribute('PICK\0{}\0{}'.format(parse[1], id))
           send(self.sockets[self.turn], 'TURN')
-          if self.room.checkBingo():
+          (end, winners) = self.room.checkBingo()
+          if end:
+            for id in winners:
+              self.distribute('BING\0{}'.format(id))
             break
         else:
           send(conSock, 'MESS\0{}\0{}'.format('Server', 'It\'s not your turn now.'))
       elif parse[0] == 'MESS':
         j = self.dict[parse[1]]
         send(self.sockets[j], 'MESS\0{}\0{}'.format(id, parse[2]))
+      elif parse[0] == 'DONE':
+        break
 
     return
 
@@ -57,35 +62,27 @@ class Server :
       send(sock, msg)
 
   def service(self):
-    for i in range(0, 5):
-      conSock, addr = self.serverSock.accept()
-      self.sockets.append(conSock)
-      t = threading.Thread(target=self.handleClient, args=(i, conSock, addr))
-      t.daemon = True
-      self.threads.append(t)
-      t.start()
-    send(self.sockets[self.turn], 'TURN')
-    for t in self.threads:
-      t.join()
-    self.roomNum = self.roomNum + 1
-    self.room = Room('room #{}'.format(self.roomNum))
-    self.threads = []
-    self.sockets = []
+    while True:
+      for i in range(0, self.playerN):
+        conSock, addr = self.serverSock.accept()
+        self.sockets.append(conSock)
+        t = Thread(target=self.handleClient, args=(i, conSock, addr))
+        t.daemon = True
+        self.threads.append(t)
+        t.start()
+      send(self.sockets[self.turn], 'TURN')
+      for t in self.threads:
+        t.join()
+      self.roomNum = self.roomNum + 1
+      self.room = Room('room #{}'.format(self.roomNum))
+      self.threads = []
+      self.sockets = []
+      self.dict = {}
+      for sock in self.sockets:
+        sock.close()
 
 
 server = Server()
 
 
 server.service()
-#server.room.addPlayer('per')
-#server.room.addPlayer('ma')
-#server.room.addPlayer('ri')
-#server.room.addPlayer('o')
-#
-#
-#list = random.sample(range(1, 100), 99)
-#for i in list:
-#  server.room.pickNumber(i)
-#  if server.room.checkBingo():
-#    server.room.printStatus()
-#    break
