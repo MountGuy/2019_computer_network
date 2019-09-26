651ac73e2a89ee658af70743a30e67cef4a35537
MountGuy <riman314@snu.ac.kr>
2019-09-26 17:47:17 +0900

    choose room

--- pseudoClient.py
+++ pseudoClient.py
@@ -1,5 +1,4 @@
-from threading import Thread
-from protocol import send, recieve, newSocket
+from protocol import send, recieve, newSocket, port
 
 serverIP = '147.46.240.95'
 localIP = '127.0.0.1'
@@ -7,8 +6,15 @@ localIP = '127.0.0.1'
 class PseudoClient:
   def __init__(self, id):
     self.clientSock = newSocket()
-    self.clientSock.connect((localIP, 8080))
-    send(self.clientSock, id)
+    self.clientSock.connect((localIP, port))
+    self.id = id
+    send(self.clientSock, 'ID\0{}'.format(id))
+
+    data = recieve(self.clientSock)
+    roomNames = data.split('\0')[1:]
+    room = roomNames[0]
+    send(self.clientSock, 'ROOM\0{}'.format(room))
+
     data = recieve(self.clientSock)
     strs = data.split()
     self.numbers = list(map(lambda x : int(x), strs))
@@ -23,11 +29,13 @@ class PseudoClient:
         for i in range(25):
           if self.numbers[i] == n:
             self.selected[i] = True
-      elif parse[0] == 'DONE':
-        send(self.clientSock, 'DONE')
-        break
+      elif parse[0] == 'BINGO':
+        print('bingo message recieved')
       elif parse[0] == 'TURN':
         for i in range(25):
           if self.selected[i] == False:
             send(self.clientSock, 'PICK\0{}'.format(self.numbers[i]))
+      elif parse[0] == 'DONE':
+        send(self.clientSock, 'DONE')
+        break
     return
