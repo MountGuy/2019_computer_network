8de4c91503bf9e086b6f014df068ce165fc5fe43
MountGuy <riman314@snu.ac.kr>
2019-10-03 02:37:15 +0900

    comments

--- protocol.py
+++ protocol.py
@@ -1,18 +1,25 @@
 import socket
 
+### port number which is shared between client and server
 port = 20332
 
+### send wrapping function
 def send(sock, msg):
+  ### send 4 characters which means the length of body first
   length = '{:4d}'.format(len(msg))
   sock.send(length.encode('utf-8'))
+
+  ### then send the body
   sock.send(msg.encode('utf-8'))
 
 def recieve(sock):
+  ### read 4 characters from socket first
   n = int(sock.recv(4).decode('utf-8'))
-#  print('recieved message length:', n)
+
+  ### then read n characters
   string = sock.recv(n).decode('utf-8')
-#  print('recieved string:', string)
   return string
 
+### create socket wrapping function
 def newSocket():
   return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
