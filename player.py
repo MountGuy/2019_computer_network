ca9448301181af95f69b98a7bb8f762090787e93
MountGuy <riman314@snu.ac.kr>
2019-10-03 02:45:16 +0900

    remove unnecessary function & comments

--- player.py
+++ player.py
@@ -5,23 +5,15 @@ class Player:
     list = [False] * 25
     self.selectBoard = [list[0:5], list[5:10], list[10:15], list[15:20], list[20:25]]
 
-  def printBoard(self):
-    print(self.id)
-    for i in range(0, 5):
-      for j in range(0, 5):
-        if self.selectBoard[i][j] == False:
-          print('{:2d} '.format(self.numberBoard[i][j]), end = '')
-        else:
-          print('[] ', end = '')
-      print()
-
-  def packBoard(self):
+  ### returns a string of board to send with socket
+  def packedBoard(self):
     string = ['', '', '', '', '']
     board = self.numberBoard
     for i in range(0, 5):
       string[i] = '{} {} {} {} {}\n'.format(board[i][0], board[i][1], board[i][2], board[i][3], board[i][4])
     return '{}{}{}{}{}'.format(string[0], string[1], string[2], string[3], string[4])
 
+  ### returns a string of nth line of board
   def nthLine(self, n):
     result = ''
     for i in range(5):
@@ -31,6 +23,7 @@ class Player:
         result += '[] '
     return result
 
+  ### basic functions of bingo game
   def pickNumber(self, n):
     for i in range(0, 5):
       for j in range(0, 5):
